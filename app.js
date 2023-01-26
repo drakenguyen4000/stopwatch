3eb9e2e1a005e9c11840fc94bdda4d0b50c1f0d6
Drake Nguyen <drakeknguyen@yahoo.com>
2023-01-25 21:22:03 -0800

    update static digits with variables

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
+//Setup baseline style
+const body = document.querySelector("body");
+body.style.fontFamily = "Orbitron, sans-serif";
+
+//Watch Digits
+const digits = {
+  h_seconds: 0,
+  t_seconds: 0,
+  seconds: 0,
+  tSeconds: 0,
+  minute: 0, 
+  tMinute: 0,
+}
+
+let tMin = digits.tMinute;
+let min= digits.minute;
+let tSec= digits.tSeconds;
+let sec= digits.seconds;
+let t_sec= digits.t_seconds;
+let h_sec= digits.h_seconds;
+
 //Create main container
 document.body.innerHTML = `<div id="container"></div>`;
 
 const container = document.getElementById("container");
-container.innerHTML = `<div id="watch"><p class="watch_text">fossil</p><p id="watch_time" class="watch_text" >00:00</p><button class="btn btn--start">Start</button><button class="btn btn--stop">Stop</button><button class="btn btn--reset">Reset</button></div>`;
+container.innerHTML = `<div id="watch"><p class="watch_text">fossil</p><p id="watch_time" class="watch_text" >${tMin}${min}:${tSec}${sec}:${t_sec}${h_sec} </p><button class="btn btn--start">Start</button><button class="btn btn--stop">Stop</button><button class="btn btn--reset">Reset</button></div>`;
 container.style.height = "500px";
 container.style.width = "500px";
 container.style.border = "20px solid #afafaf";
@@ -23,13 +44,13 @@ watch.style.margin = "auto";
 watch.style.borderRadius = "20px";
 
 const watch_time = document.getElementById("watch_time");
-watch_time.style.fontSize = "80px";
+watch_time.style.fontSize = "70px";
 
 //Style all buttons
 let btns = document.querySelectorAll('.btn'), i;
 for (i = 0; i < btns.length; ++i) {
-  btns[i].style.color = "green";
   btns[i].style.padding = "3% 8%";
+  btns[i].style.cursor = "pointer";
   btns[i].style.background = "none";
   btns[i].style.border = "2px solid lightblue";
   btns[i].style.margin = "0 1%";
@@ -37,3 +58,4 @@ for (i = 0; i < btns.length; ++i) {
   btns[i].style.fontSize = "20px";
   btns[i].style.borderRadius = "10px";
 }
+
